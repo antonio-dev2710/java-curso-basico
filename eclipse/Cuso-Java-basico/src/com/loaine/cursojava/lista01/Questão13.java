30f8bb60fe916cab859cf2f0a7c97be746292aa2
Antonio Alves <antoniocunha9610@gmail.com>
2022-04-14 19:01:15 -0300

    Att

---
+++ eclipse/Cuso-Java-basico/src/com/loaine/cursojava/lista01/Questão13.java
@@ -0,0 +1,36 @@
+package com.loaine.cursojava.lista01;
+
+import java.util.Scanner;
+
+public class Questão13 {
+
+	public static void main(String[] args) {
+		
+
+		Scanner scan=new Scanner(System.in);
+		//responda true ou false
+		System.out.print("Vc é mulher?");
+		boolean genero=scan.nextBoolean();
+		
+		System.out.print("Qual é a sua altura?");
+		double altura=scan.nextDouble();
+		double pesoIdeal;
+		if(genero==true) {
+			//(62.1*h) - 44.7
+			
+			pesoIdeal=(62.1*altura) - 44.7;
+			
+			System.out.print("Seu peso ideal é: "+pesoIdeal);
+			
+		}
+		else {
+			//(72.7*h) - 58
+			pesoIdeal=(72.7*altura) - 58;
+
+			System.out.print("Seu peso ideal é: "+pesoIdeal);
+			
+		}
+		
+	}
+
+}
